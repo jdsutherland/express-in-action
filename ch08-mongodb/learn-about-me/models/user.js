51271553dadf8048a6b7125c05d342d255dbe0a5
Jeff Sutherland <jdsuther@asu.edu>
2017-02-20 18:07:04 +0700

    Add secure password checking to User model

--- ch08-mongodb/learn-about-me/models/user.js
+++ ch08-mongodb/learn-about-me/models/user.js
@@ -23,4 +23,25 @@ const userSchema = mongoose.Schema({
 
 const noop = () => {};
 
+userSchema.pre('save', (done) => {
+  if (!this.isModified('password')) { return done(); }
+
+  bcrypt.genSalt(SALT_FACTOR, (err, salt) => {
+    if (err) { return done(err); }
+    bcrypt.hash(this.password, salt, noop, (err1, hashedPassword) => {
+      if (err1) { return done(err1); }
+      this.password = hashedPassword;
+      done();
+    });
+  });
+});
+
+userSchema.methods.checkPassword = (guess, done) => {
+  bcrypt.compare(guess, this.password, (err, isMatch) => {
+    done(err, isMatch);
+  });
+};
+
 userSchema.methods.name = () => this.displayName || this.username;
+
+module.exports = mongoose.model('User', userSchema);
