545cec630e6f183a2a7b05433f1dd8b1c6143eda
Jeff Sutherland <jdsuther@asu.edu>
2017-02-21 16:46:48 +0700

    Add html response tests
    Testing complete

--- ch09-testing/whats-my-user-agent-tdd/app.js
+++ ch09-testing/whats-my-user-agent-tdd/app.js
@@ -3,14 +3,21 @@ const path = require('path');
 
 const app = express();
 
-const viewsPath = path.join(__dirname, 'views');
 app.set('port', process.env.PORT || 3000);
+
+const viewsPath = path.join(__dirname, 'views');
 app.set('view engine', 'ejs');
 app.set('viewsPath', viewsPath);
 
 app.get('/', function(req, res){
-  res.type('text');
-  res.send(req.get('User-Agent'));
+  const userAgent = req.get("user-agent") || "none";
+
+  if (req.accepts('html')) {
+    res.render('index', { userAgent });
+  } else {
+    res.type('text');
+    res.send(userAgent);
+  }
 });
 
 const server = app.listen(app.get('port'), function() {
---
+++ ch09-testing/whats-my-user-agent-tdd/test/html.js
@@ -0,0 +1,32 @@
+var app = require("../app");
+
+var supertest = require("supertest");
+
+describe("plain text response", () => {
+
+  var request;
+  beforeEach(() => {
+    request = supertest(app)
+      .get("/")
+      .set("User-Agent", "a cool browser")
+      .set("Accept", "text/plain");
+  });
+
+  it("returns a plain text response", (done) => {
+    request
+      .expect("Content-Type", /text\/plain/)
+      .expect(200)
+      .end(done);
+  });
+
+  it("returns your User Agent", (done) => {
+    request
+      .expect((res) => {
+        if (res.text !== "a cool browser") {
+          throw new Error("Response does not contain User Agent");
+        }
+      })
+      .end(done);
+  });
+
+});
