560bdc4dc88ec9b019418dee5c7a7b57bef936c6
Jeff Sutherland <jdsuther@asu.edu>
2017-02-21 15:49:15 +0700

    Add more tests

--- ch09-testing/simple-tests/test/capitalize.js
+++ ch09-testing/simple-tests/test/capitalize.js
@@ -7,6 +7,31 @@ const expect = chai.expect;
 describe('capitalize', function() {
   it('capitalizes single words', function() {
     expect(capitalize('express')).to.eql('Express');
-    expect(capitalize('cats')).to.eql('Cats');
+  });
+
+  it('makes the rest of the string lowercase', function() {
+    expect(capitalize('exPRESs')).to.eql('Express');
+  });
+
+  it('leaves empty strings alone', function() {
+    expect(capitalize('')).to.eql('');
+  });
+
+  it('leaves an already capitalized words alone', function() {
+    expect(capitalize('Express')).to.eql('Express');
+  });
+
+  it('leaves a string with no words alone', function() {
+    expect(capitalize('  ')).to.eql('  ');
+    expect(capitalize('123')).to.eql('123');
+  });
+
+  it('capitalizes multi-word strings', function() {
+    expect(capitalize('what is express?')).to.eql('What is express?');
+    expect(capitalize('i love express')).to.eql('I love express');
+  });
+
+  it("doesn't blow up when starting with a number", function() {
+    expect(capitalize('21 grams')).to.eql('21 grams');
   });
 });
